233ab223f65a73894e77f09e7385fdac344aeb49
Chris Huxtable <chris@huxtable.ca>
2020-01-18 16:46:45 -0500

    Adds support for Crystal 0.29.0
    Good to at lease 0.32.1

--- spec/slice_spec.cr
+++ spec/slice_spec.cr
@@ -22,25 +22,25 @@ describe "Slice" do
     slice_3 = "jkl".to_slice
 
     ptr = Pointer(Pointer(UInt8)).malloc(6)
-    ptr[0] = slice_0.pointer(0)
-    ptr[1] = slice_1.pointer(0)
-    ptr[2] = slice_2.pointer(0)
-    ptr[3] = slice_3.pointer(0)
+    ptr[0] = slice_0.to_unsafe()
+    ptr[1] = slice_1.to_unsafe()
+    ptr[2] = slice_2.to_unsafe()
+    ptr[3] = slice_3.to_unsafe()
     ptr[4] = Pointer(UInt8).null
 
     slice = Slice.new(ptr, limit: 8, read_only: true)
     slice.size.should eq(4)
-    slice[0].should eq(slice_0.pointer(0))
-    slice[1].should eq(slice_1.pointer(0))
-    slice[2].should eq(slice_2.pointer(0))
-    slice[3].should eq(slice_3.pointer(0))
+    slice[0].should eq(slice_0.to_unsafe())
+    slice[1].should eq(slice_1.to_unsafe())
+    slice[2].should eq(slice_2.to_unsafe())
+    slice[3].should eq(slice_3.to_unsafe())
 
     slice = Slice.new(ptr, limit: 4, read_only: true)
     slice.size.should eq(4)
-    slice[0].should eq(slice_0.pointer(0))
-    slice[1].should eq(slice_1.pointer(0))
-    slice[2].should eq(slice_2.pointer(0))
-    slice[3].should eq(slice_3.pointer(0))
+    slice[0].should eq(slice_0.to_unsafe())
+    slice[1].should eq(slice_1.to_unsafe())
+    slice[2].should eq(slice_2.to_unsafe())
+    slice[3].should eq(slice_3.to_unsafe())
 
     expect_raises(Slice::MissingTerminatorError) { Slice.new(ptr, limit: 3, read_only: true) }
     expect_raises(Slice::MissingTerminatorError) { Slice.new(ptr, limit: 2, read_only: true) }
